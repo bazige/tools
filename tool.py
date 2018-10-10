ac9c4c78553bd83156b42ebe30b79301f3a3a1ec
bazige <1165712867@qq.com>
2018-10-10 16:46:24 +0800

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import os
+import shutil
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
+import argparse
+import json
+
+cls = {"traffic_passerby_rect": 0,
+       "traffic_passerbyhard_rect": 0,
+       "traffic_personside_rect": 1,
+       "traffic_smallcar_rect": 1,
+       "traffic_smallcarhard_rect": 1,
+       "traffic_minubus_rect": 1,
+       "traffic_bus_rect": 1,
+       "traffic_motorcycle_rect": 2,
+       "traffic_motorcyclehard_rect": 2,
+       "traffic_tricycle_rect": 2,
+       "traffic_tricyclehard_rect": 2,
+       "traffic_allcar_rect": 1,
+       "traffic_allcarhard_rect": 1,
+       "traffic_motorcycle_nohuman_rect": 3,
+       "traffic_bicycle_nohuman_rect": 3
+       }
+       
+traffic_list = ['ImageName', 'TaskId', 'Filesum']
+
+def create
+       
+       
+       
+       
