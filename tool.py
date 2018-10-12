881e70acdfc9f10a3510182bed4058ba7af69db9
bazige <1165712867@qq.com>
2018-10-12 17:53:36 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -93,8 +93,63 @@ def parse_json(img_path, mode='darknet'):
                     # print root
                     # print doc
                     file_path = os.path.join(root, doc)
+                    f = open(file_path, 'r')
+                    json_f = json.load(f)
+                    img = cv2.imread(file_path[:-5])
+                    if img is None:
+                        continue
+                    print file_path[:-5]
+                    if mode == "darknet":
+                        with open(file_path[:-9] + '.txt', 'w') as g:
+                            for key in json_f.keys():
+                                if key not in traffic_list:
+                                    if key == 'HumanFace_face_rect' or key == 'behavior_other_animal_rect':
+                                        continue
+                                    elif key not in cls.keys():
+                                        print key
+                                        print '    error:This key is not in list!'
+                                        print ''
+                                        break
+                                    print key
+                                    sub = json_f[key]
+                                    for index in xrange(len(sub)):
+                                        if 'rect' not in sub[index].keys():
+                                            continue
+                                        x_1 = int(sub[index]['rect'][0][0])
+                                        y_1 = int(sub[index]['rect'][0][1])
+                                        x_2 = int(sub[index]['rect'][1][0])
+                                        y_2 = int(sub[index]['rect'][1][1])
+                                        x = (x_1 + x_2) / 2.0 / 8192
+                                        y = (y_1 + y_2) / 2.0 / 8192
+                                        w = (x_2 - x_1) / 8192
+                                        h = (y_2 - y_1) / 8192
+                                        g.write(str(cls[key]) + ' ' + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + '\n')
+                    elif mode == "oc":
+                        save_path = '/home/cc'
+                        for key in json_f.keys():
+                            if key not in traffic_list:
+                                if key not in cls.keys():
+                                    print key
+                                    break
+                                key_path = os.path.join(save_path, key)
+                                if not os.path.isdir(key_path):
+                                    os.makedirs(key_path)
+                                print key
+                                sub = json_f[key]
+                                for index in xrange(len(sub)):
+                                    if 'rect' not in sub[index].keys():
+                                        continue
+                                    x_1 = int(int(sub[index]['rect'][0][0]) * img.shape[1] / 8192.0)
+                                    y_1 = int(int(sub[index]['rect'][0][0]) * img.shape[1] / 8192.0)
+                                    y_1 = int(int(sub[index]['rect'][0][0]) * img.shape[1] / 8192.0)
+                                    y_1 = int(int(sub[index]['rect'][0][0]) * img.shape[1] / 8192.0)
+                                    sub_img = img[y_1:y_2, x_1:x_2]
+                                    sub_path = os.path.join(key_path, doc[:-9] + '_' + str(index) + ".jpg")
+                                    cv2.imwrite(subpath, sub_img)
                                       
-                                      
-                                      
-                                      
-       
+if __name__ == '__main__':
+    parent_parser = argparse.ArgumetnParser(description='tool script for daily use")
+    sub_parser = parent_parser.add_subparsers(title='subcommonds')
+                                            
+    parser_create_list = sub_parser.add_parser('create')
+    parser_create_list.add_argument('img_path', helo
